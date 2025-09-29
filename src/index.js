43a7d6aa3aab9e6b4006b2d90b8f79112eb3fc18
Chris <47066922+cwilso28@users.noreply.github.com>
2025-09-28 21:42:21 -0500

    Initialize api fetch to visual crossing

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import "./styles.css"
 
 import { api_key } from './config.js'
 
-console.log(api_key);
+let location = '68521'
+
+fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=${api_key}`)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        console.log(response);
+    });
