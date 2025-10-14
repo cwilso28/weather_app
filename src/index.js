8c93c96591dfef6c16eab11bfa8423d00491a686
Chris <47066922+cwilso28@users.noreply.github.com>
2025-10-13 21:37:31 -0500

    Add JSON save function and url builder
    A new function is created to save a JSON file to the localStorage in the
    browser. The url builder creates the url upon class generation so that
    each class will have its own associated weather data to pull from. This
    will help when multiple locations are being accessed.

--- src/index.js
+++ src/index.js
@@ -15,22 +15,22 @@ let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/ser
 //         console.log(response);
 //     });
 
-async function apiRequest(url, save = true) {
+async function apiRequest(url) {
     let response = await fetch(url)
     console.log("API fetch request sent.")
     
     if (response.ok) {
         let responseJSON = await response.json();
 
-        if (save) {
-            localStorage.apiResponse = JSON.stringify(responseJSON);
-        };
-
         return responseJSON;
     };
 };
 
-function loadJSON() {
+function saveJSON(json) {
+    localStorage.apiResponse = JSON.stringify(json);
+}
+
+function loadJSON(url) {
     if (localStorage.apiResponse) {
         console.log("Data read from storage");
         return JSON.parse(localStorage.getItem("apiResponse"));
@@ -41,15 +41,19 @@ function loadJSON() {
     };
 };
 
-let response = loadJSON();
-console.log(response);
-
 class weatherRequest {
-    constructor(location, date1='', date2='') {
+    constructor(location, date1='', date2='', dev = true) {
         this.location = location;
         this.date1 = date1;
         this.date2 = date2;
+        this.dev = dev;
     };
+
+    createURL() {
+        let parameterArray = [this.location, this.date1, this.date2];
+        let parameterString = parameterArray.filter(Boolean).join("/");
+        return `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${parameterString}/?key=${api_key}`
+    }
 
     emptyVariableCheck() {
         if (this.date1) {
@@ -63,5 +67,5 @@ class weatherRequest {
 };
 }
 
-// let test = new weatherRequest(location);
-// test.emptyVariableCheck();
+let test = new weatherRequest(location);
+test.createURL();
